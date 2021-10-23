dd634cb2ba4513e85f1aa6c1b9fce7fc7f16e446
verdipratama <verdipratama@icloud.com>
2021-10-23 09:10:05 +0700

    refactor model search filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
@@ -12,13 +12,16 @@ class Post extends Model
 
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
-    protected $with = ['category', 'author'];
+    protected $with    = ['category', 'author'];
 
     public function scopeFilter($query, array $filters)
     {
 
         $query->when($filters['search'] ?? false, function ($query, $search) {
-            return $query->where('title', 'like', '%' . $search . '%')->orWhere('body', 'like', '%' . $search . '%');
+            return $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search . '%');
+            });
         });
 
         $query->when($filters['category'] ?? false, function ($query, $category) {
@@ -53,8 +56,8 @@ class Post extends Model
     {
         return [
             'slug' => [
-                'source' => 'title'
-            ]
+                'source' => 'title',
+            ],
         ];
     }
 }
